842f9dd22ff7c1c8905484458686935a4c80bccd
Rich Chiodo <rchiodo@microsoft.com>
2018-10-02 13:59:53 -0700

    Move all the strings into the package.nls.<loc>.json files. No need to copy files to output and no need for extra json files

--- src/utils/localize.ts
+++ src/utils/localize.ts
@@ -5,6 +5,7 @@
 
 import * as fs from 'fs';
 import * as path from 'path';
+import { EXTENSION_ROOT_DIR } from '../client/common/constants';
 
 // External callers of localize use these tables to retrieve localized values.
 export namespace LanguageServiceSurveyBanner {
@@ -16,6 +17,7 @@ export namespace LanguageServiceSurveyBanner {
 // Skip using vscode-nls and instead just compute our strings based on key values. Key values
 // can be loaded out of the nls.<locale>.json files
 let loadedCollection: { [index: string]: string } | undefined ;
+let defaultCollection: { [index: string]: string } | undefined ;
 let loadedLocale: string;
 
 function localize(key: string, defValue: string) {
@@ -34,12 +36,17 @@ function parseLocale() : string {
 function getString(key: string, defValue: string) {
     // Load the current collection
     if (!loadedCollection || parseLocale() !== loadedLocale) {
-        loadedCollection = load();
+        load();
     }
 
-    // Lookup the string by the key
+    // First lookup in the dictionary that matches the current locale
     if (loadedCollection && loadedCollection.hasOwnProperty(key)) {
         return loadedCollection[key];
+    }
+
+    // Fallback to the default dictionary
+    if (defaultCollection && defaultCollection.hasOwnProperty(key)) {
+        return defaultCollection[key];
     }
 
     // Not found, return the default
@@ -51,14 +58,25 @@ function load() {
     loadedLocale = parseLocale();
 
     // Find the nls file that matches (if there is one)
-    const nlsFile = path.join(__dirname, 'i18n', `localize.nls.${loadedLocale}.json`);
+    const nlsFile = path.join(EXTENSION_ROOT_DIR, `package.nls.${loadedLocale}.json`);
     if (fs.existsSync(nlsFile)) {
         const contents = fs.readFileSync(nlsFile, 'utf8');
-        return JSON.parse(contents);
+        loadedCollection = JSON.parse(contents);
+    } else {
+        // If there isn't one, at least remember that we looked so we don't try to load a second time
+        loadedCollection = {};
     }
 
-    // If there isn't one, at least remember that we looked so we don't try to load a second time
-    return {};
+    // Get the default collection if necessary. Strings may be in the default or the locale json
+    if (!defaultCollection) {
+        const defaultNlsFile = path.join(EXTENSION_ROOT_DIR, 'package.nls.json');
+        if (fs.existsSync(defaultNlsFile)) {
+            const contents = fs.readFileSync(defaultNlsFile, 'utf8');
+            return JSON.parse(contents);
+        } else {
+            defaultCollection = {};
+        }
+    }
 }
 
 // Default to loading the current locale
